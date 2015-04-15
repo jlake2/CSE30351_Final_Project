7edeff69588a7f002de5aa41c57d8b62af23332c
John Frederick Lake <jlake2@student00.cse.nd.edu>
2015-04-15 15:02:45 -0400

    Initial setup of Turing Machine almost done

--- tm.py
+++ tm.py
@@ -7,7 +7,7 @@ import re
 
 
 #This will get all 6 Parts of the DPDA:
-def getDPDA(filename):
+def getTM(filename):
 	numTransitions =0
 	trans={}
 	n = {}
@@ -23,12 +23,12 @@ def getDPDA(filename):
 		
 			n['alphabet'] = alpha
 		elif(line[0] == 'Z'):
-			#Get the stack alphabet. 
-			salpha = []
-			salpha = re.split(',|\n', line[2:-1])
-			if len(salpha) == 1 and salpha[0] == "":
+			#Get the tape alphabet. 
+			talpha = []
+			talpha = re.split(',|\n', line[2:-1])
+			if len(talpha) == 1 and talpha[0] == "":
 				return -1
-			n['salphabet'] = salpha
+			n['talphabet'] = talpha
 
 		elif(line[0] == 'Q'):
 			#Get the states:
@@ -47,21 +47,21 @@ def getDPDA(filename):
 			#Place the transition function inside a list
 			# t[0] is the starting point
 			# t[1] is the input
-			# t[2] is the expected item @ top of stack
-			# t[3] is the next state
-			# t[4] is the item pushed onto stack
+			# t[2] is the next state
+			# t[3] is the value to be written
+			# t[4] is the direction to go
 			t = []
 			t = re.split(',|\n', line[2:-1])
 			if len(t) == 1 and t[0] == "":
 				return -1
-			elif t[0] not in n['states'] or t[3] not in n['states']:
+			elif t[0] not in n['states'] or t[2] not in n['states']:
 				return -1	
-			elif t[1] not in n['alphabet'] and t[1] != 'e':
+			elif t[1] not in n['talphabet']:
 				return -1
-			elif t[2] not in n['salphabet'] and t[2] != 'e':
+			elif t[3] not in n['talphabet']:
 				return -1
-			elif t[4] not in n['salphabet'] and t[4] != 'e':
-				return -1
+			elif t[4] is not 'R' and t[4] is not 'L':
+				return -2
 
 			trans[numTransitions] = t
 
@@ -82,6 +82,8 @@ def getDPDA(filename):
 			f = re.split(',|\n', line[2:-1])
 			if len(f) == 1 and f[0] == "":
 				return -1
+			elif len(f) is not 2:
+				return -1
 
 			n['acceptStates'] = f
 		else:
@@ -91,7 +93,7 @@ def getDPDA(filename):
 	return n
 
 
-def processInput(dpda):
+def processInput(tm):
 
  	numInputTapes = int(raw_input(""))
 
@@ -108,136 +110,30 @@ def processInput(dpda):
 		validInput=1;
 
 		#Set the css to whatever the start state is: 
-		cs = dpda['start'][0]
+		cs = tm['start'][0]
 
 
 
-		if len(inputTape) == 1 and inputTape[0] == "":
-			for key in dpda['transitions']:
-				trans = dpda['transitions'][key]
-				#The transition is applicable to the state we are in: 
-				if trans[0] == cs:
-
-					#Automatic transition (always check first.)
-					if trans[1] == 'e' and trans[2] == 'e':
-						cs = trans[3]
-						if trans[4] is not 'e':
-							stack.append(trans[4])
-							stack.reverse()
-							print cs+"; "+trans[1]+"; "+trans[2]+"; "+trans[3]+"; "+",".join(stack)
-							stack.reverse()
-			acc = 0;
-			if cs in dpda['acceptStates']:
-					acc=1;
-			if acc == 1:
-				print "ACCEPT!\n"
-			else:
-				print "REJECT!\n"
-			continue
-				
-		for key in dpda['transitions']:
-			trans = dpda['transitions'][key]
-
-			#The transition is applicable to the state we are in: 
-			if trans[0] == cs:
-				if trans[1] == 'e' and trans[2] == 'e':
-					stack.reverse()
-					print cs+"; "+trans[1]+"; "+trans[2]+"; "+trans[3]+"; "+",".join(stack)+trans[4]
-					stack.reverse()
-					cs = trans[3]
-					if trans[4] is not 'e':
-						stack.append(trans[4])
-			
-
-		for input in inputTape:
-			if input in dpda['alphabet']:
-				for key in dpda['transitions']:
-					trans = dpda['transitions'][key]
-					#The transition is applicable to the state we are in: 
-					if trans[0] == cs:
-
-						#Automatic transition (always check first.)
-						if trans[1] == 'e' and trans[2] == 'e':
-							cs = trans[3]
-							if trans[4] is not 'e':
-								stack.append(trans[4])
-								stack.reverse()
-								print cs+"; "+trans[1]+"; "+trans[2]+"; "+trans[3]+"; "+",".join(stack)
-								stack.reverse()
-	
-
-						#Doesn't matter what's on the stack: 
-						elif trans[2] == 'e':
-							if trans[1] == input:
-								cs = trans[3]
-								if trans[4] is not 'e':
-									stack.append(trans[4])
-								stack.reverse()
-								print cs+"; "+trans[1]+"; "+trans[2]+"; "+trans[3]+"; "+",".join(stack)
-								stack.reverse()
-								break
-		
-						#Doesn't matter what the input is
-						elif trans[1] == 'e':
-							stackTop = stack.pop()
-							if trans[2] == stackTop:
-								cs = trans[3]
-								if trans[4] is not 'e':
-									stack.append(trans[4])
-								stack.reverse()
-								print cs+"; "+trans[1]+"; "+trans[2]+"; "+trans[3]+"; "+",".join(stack)
-								stack.reverse()
-								break
-							else:
-								stack.append(stackTop)
 						
-				
-						else:
-							stackTop = stack.pop()
-							if trans[2] == stackTop and trans[1] == input:
-								cs = trans[3]
-								if trans[4] is not 'e':
-									stack.append(trans[4])
-								stack.reverse()
-								print cs+"; "+trans[1]+"; "+trans[2]+"; "+trans[3]+"; "+",".join(stack)
-								stack.reverse()
-							else:
-								stack.append(stackTop)
-			else: 
-				print "REJECT: This isn't recognized by the alphabet of this DPDA!"
-			
-
-
-
-			
-		#Either accept or reject the given input: 
-		if(validInput == 1):
-			accept = 0;	
-			if(cs in dpda['acceptStates']):
-				accept = 1
-			if(accept == 1):
-				print "ACCEPT!\n"
-			else: 
-				print "REJECT!\n"
-			
-			
 				
 				
 	
 
 def main(argv):
 	#DPDA is a dictionary. Set it up:
-	dpda = {}
-	dpda = getDPDA(argv[1])	
+	tm = {}
+	tm = getTM(argv[1])	
+	for key,value in tm.iteritems():
+		print tm[key]
 
 
 
-	if(dpda == -1):
+	if(tm == -1):
 		print "ERROR, INVALID DPDA INPUT!"
 	else:
-		if(dpda != 0):
+		if(tm != 0):
 			#There is a valid NFA: 
-			processInput(dpda)
+			processInput(tm)
 
 
 
